a1a9ede93bb2d7661cc025e3ee08a8c395a03ac3
Pankaj-bit361 <pnkjvshsht1@gmail.com>
2022-10-19 15:59:58 +0530

    check the prime number

---
+++ prime.js
@@ -0,0 +1,11 @@
+let num=13;
+var count=0;
+    for(var i=1;i<=num;i++){
+        if(num%2===0){
+            count++;
+        }
+    }if(count==1){
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }
